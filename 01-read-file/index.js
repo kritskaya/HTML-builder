042c4ef5b1a6aa466fcb3709f2b7e7ca7ab2d4c1
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-05-12 15:16:12 +0300

    feat: implement task 01

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+
+const stream = fs.createReadStream(path.join(__dirname,'text.txt'));
+
+stream.on('data', (data) => {
+	console.log(data.toString());
+});
