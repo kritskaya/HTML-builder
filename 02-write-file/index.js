d930c6790831bb3098ca868fa986e65301cf8c06
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-05-13 15:44:19 +0300

    feat: implement task 02

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,2 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+
+
+const file = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+const rl = readline.createInterface({ input, output });
+
+process.on('exit', () => {
+	console.log('Программа прекращает работу');
+});
+
+
+const recursiveReadLine = () => {
+	rl.question('Введите текст для записи в файл:\n', (answer) => {
+		if (answer === 'exit') {
+			return rl.close(); 
+		}
+		
+		file.write(`${answer}\n`);
+		recursiveReadLine(); 
+	});
+};
+
+recursiveReadLine();
+
+
 
 
